d9bd1abde6653edaaaf4e024c8f6334a287459b5
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-08-19 12:10:14 +0530

    Added Constants

---
+++ src/constants.js
@@ -0,0 +1,40 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Dinesh';
+
+export const experiences = [
+    {
+        'company': '7Webs',
+        'role': 'Software Developer Intern',
+        'duration': 'May 2024 - July 2024',
+        'logo': 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+        'points': [
+            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': 'https://inncircles.com/#home',
+    },
+
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
